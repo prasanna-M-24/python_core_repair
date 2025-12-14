be214559adc3aadb35e88dce283a183d6671185a
prasanna-M-24 <prasannameesalah9@gmail.com>
2025-12-14 10:14:53 +0530

    revised code

--- day0_sanity_check.py
+++ day0_sanity_check.py
@@ -1,12 +1,15 @@
 def fun(num):
+    odd_count=0
+    even_count=0
     for i in range (1,num+1):
-        print(i)
-    if num%2==0: 
-            print("no. of odds:",num//2)
-            print("no.of even:",num//2)
-    else: 
-            print("no od odds:",num//2+1)
-            print("no.of even:",num-(num//2+1))
-
+        if i%2==0: 
+            even_count+=1
+            print(i,"-> Even")
+        else:
+            odd_count+=1
+            print(i,"-> Odd")
+    print("total odds:",odd_count)
+    print("total even:",even_count)
+            
 num=int(input("Enter any number:"))
 fun(num)
