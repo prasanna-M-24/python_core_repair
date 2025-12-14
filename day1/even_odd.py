283bb598ec5276a95b97b6eb19d6905ecce5b733
prasanna-M-24 <prasannameesalah9@gmail.com>
2025-12-14 11:07:51 +0530

    finding whether the entered number is even or odd

---
+++ day1/even_odd.py
@@ -0,0 +1,8 @@
+def Even_odd(num):
+    if num%2 == 0:
+        print("Entered number is even")
+    else:
+        print("Entered number is odd")
+
+num=int(input("Enter a number:"))
+Even_odd(num)
